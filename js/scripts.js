b1644c586cbb67c6f2a7181593549dacafafbafc
Pawel Morka <morka.paul@gmail.com>
2018-02-08 17:54:52 +0100

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 var buttons = document.getElementsByClassName('button');
+var size = buttons.length;
 
-for (var i=0; i<buttons.length; i++) {
+for (var i = 0; i < size; i++) {
 	alert(buttons[i].innerText);
 	console.log(buttons[i].innerHTML);
 }
